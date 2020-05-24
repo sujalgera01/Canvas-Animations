107c17365e7962128ec3c99a35bdbcddaa232953
sujalgera01 <sujalgera01@gmail.com>
2020-05-24 17:46:02 +0530

    changes

--- play.js
+++ play.js
@@ -2,9 +2,12 @@ var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext('2d');
 
 var x=0;
+var y=0;
+var dx=1
 setInterval(function(){
 
-	x++;
+	x = x+dx;
+	y++;
 	ctx.fillStyle = "black";
 	ctx.fillRect(0,0,canvas.width, canvas.height);
 	ctx.fill();
@@ -13,5 +16,18 @@ setInterval(function(){
 	ctx.arc(x,220,75,0,Math.PI*2);
 	ctx.fillStyle = "white";
 	ctx.fill();
+	
 
-},10);
+	ctx.beginPath();
+	ctx.arc(130,y,50,0,Math.PI*2);
+	ctx.fillStyle = "blue";
+	ctx.fill();
+
+
+	ctx.beginPath();
+	ctx.arc(x,y,100,0,Math.PI*2);
+	ctx.fillStyle = "red";
+	ctx.fill();
+
+
+});
